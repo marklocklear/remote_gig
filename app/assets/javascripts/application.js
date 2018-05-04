ae86f72fd8a0a56ecdaec0c07b52804185a42835
Mark Locklear <marklocklear@gmail.com>
2018-05-04 11:36:38 -0400

    add back to top functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,14 +15,23 @@
 //= require jquery
 //= require_tree .
 
-// $(document).ready(function(){
-//     $("#flip").click(function(){
-//         $("#panel").slideToggle("slow");
-//     });
-// });
-
 $(document).ready(function(){
     $(".flip").click(function(){
         $(this).next().find(".panel").slideToggle("slow");
     });
+    
+    // Check distance to top and display back-to-top.
+	$( window ).scroll( function() {
+		if ( $( this ).scrollTop() > 800 ) {
+			$( '.back-to-top' ).addClass( 'show-back-to-top' );
+		} else {
+			$( '.back-to-top' ).removeClass( 'show-back-to-top' );
+		}
+	});
+
+	// Click event to scroll to top.
+	$( '.back-to-top' ).click( function() {
+		$( 'html, body' ).animate( { scrollTop : 0 }, 800 );
+		return false;
+	});
 });
