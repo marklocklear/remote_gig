0fdcab1613cf038c3dcb5d59d78501d127ff0bde
Mark Locklear <marklocklear@gmail.com>
2018-05-16 14:09:09 -0400

    allow cors

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module RemoteGig
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+		config.action_dispatch.default_headers = {
+	    'Access-Control-Allow-Origin' => '*',
+	    'Access-Control-Request-Method' => %w{GET}.join(",")
+	  }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
