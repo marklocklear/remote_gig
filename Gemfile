a118c474653e74ffc9719a2c07a656df54214e79
Mark Locklear <marklocklear@gmail.com>
2018-08-07 14:07:28 -0400

    add whenever for cron jobs

--- Gemfile
+++ Gemfile
@@ -44,6 +44,9 @@ gem "font-awesome-rails"
 gem 'httparty'
 gem 'acts-as-taggable-on', '~> 6.0'
 
+#handles cron jobs
+gem 'whenever'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ config/schedule.rb
@@ -0,0 +1,30 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+#get fresh jobs
+every 1.day, at: '2:30 am' do
+	rake "get_jobs"
+end
+
+#send a tweet
+every 1.day, at: '2:30 pm' do
+	rake "send_tweet"
+end
