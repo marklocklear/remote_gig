62f6bd6987f0fb769b4d674332fa09d295dc7de8
Mark Locklear <marklocklear@gmail.com>
2018-11-09 15:35:41 -0500

    fix bugs in get_jobs task for redhat and clevertech

--- lib/sandbox/rh_scraper/app.rb
+++ lib/sandbox/rh_scraper/app.rb
@@ -1,18 +1,14 @@
-require 'net/http'
-require 'json'
-require "awesome_print"
+require 'httparty'
 
 url = 'https://redhat.jobs/jobs/feed/json'
-uri = URI(url)
-response = Net::HTTP.get(uri)
-jobs = JSON.parse(response)
-# jobs["city"].map {|jobs1| jobs1['city'] if jobs1['key']=='Remote'}.compact.first
-count = 0
-jobs_array = []
+response = HTTParty.get(url)
+jobs = response.parsed_response
+
+# jobs = JSON.parse(response)
+
 jobs.each do |j|
 	if j["city"] == "Remote" && j["country_short"] == "USA"
-		jobs_array << j["title"] + j["url"]
+		# Job.create_job(j["title"], j["url"], j["description"], "Redhat")
+		puts j["title"]
 	end
 end
-puts jobs_array
-# jobs[0].find {|jobs1| jobs1['key']=='Remote'}['city']
