c84532861ad5706717b05891a6af35561011ac34
Mark Locklear <marklocklear@gmail.com>
2019-01-10 15:14:38 -0500

    stop randomly ordering jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,8 +4,7 @@ class JobsController < ApplicationController
   # GET /jobs
   # GET /jobs.json
   def index
-    @jobs = Job.order_jobs
-
+    @jobs = Job.all
     if params[:search_term]
       @search_term = params[:search_term].downcase
       @jobs = Job.where("LOWER(title) LIKE ? OR LOWER(description) LIKE ?", "%#{@search_term}%", "%#{@search_term}%")
