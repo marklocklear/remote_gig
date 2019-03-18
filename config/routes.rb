04cee2fdaf352f0c0da8779cd6ee69f4ff766021
Mark Locklear <marklocklear@gmail.com>
2019-03-18 11:28:16 -0400

    write output from get_jobs so it can be view publicly

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
+  get '/stats', :to => redirect('/nightly_stats.txt')
   resources :jobs do
   	collection do
   		post :email_signup
   	end
   end
-
   get '*tag', to: 'jobs#index'
-
   root to: 'jobs#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
