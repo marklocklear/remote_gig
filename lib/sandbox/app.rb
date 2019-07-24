cff3985cf0184f836df716229c1de4faa6af50ff
Michael <michael.c.hadley@gmail.com>
2019-07-24 13:54:34 -0400

    revert for app.rb

--- lib/sandbox/app.rb
+++ lib/sandbox/app.rb
@@ -1,41 +1,29 @@
 require 'open-uri'
 require 'nokogiri'
 
-doc = Nokogiri::HTML(open("https://www.railroad19.com/#careers"))
-  jobs = doc.css('#job-postings')
-  
-  jobs.css('.job-posting').each do |char_element|
-		title = char_element.css('a').text
+doc = Nokogiri::HTML(open("https://www.7cups.com/about/jobs/"))
+	jobs = doc.css('.g-basic-list')
+
+	jobs.css('li').each do |char_element|
+		# puts char_element
+		title = char_element.text
 		if title.include? 'Remote'
-			link = char_element.css('a').first['href']
-			description = char_element.css('p')
-			company = 'Railroad19'
-			jobs_array = [title, link, description, company]
+		# puts "************"
+		# puts title
+    link = char_element.css('a').first['href']
+    # puts link
+    clickLink = "https://www.hashicorp.com" + link
+    # puts url
+		job_page = Nokogiri::HTML(open(clickLink.to_s))
+		# puts job_page.inspect
+    description = job_page.xpath('/html/body/main/div[2]/div[2]/ul[1]').text 
+    # puts description
+    # 	puts title
+      puts '######################'
+			puts title + clickLink
+      puts description
+      puts '######################'
+		# end
+    # puts url
     end
-    puts jobs_array
   end
-  
-# 	jobs.css('.job-posting').each do |char_element|
-# 		# puts char_element
-# 		title = char_element.text
-# 		# if title.include? 'Remote'
-# 		# puts "************"
-# 		# puts title
-#     # link = char_element.css('a').first['href']
-#     # puts link
-#     # clickLink = link
-#     # puts url
-# 		# job_page = Nokogiri::HTML(open(link.to_s))
-# 		# puts job_page.inspect
-#     description = char_element.css('p')
-#     # puts description
-#     # 	puts title
-#       # puts '######################'
-#       puts title 
-#       puts '***'
-#       puts description
-#       puts '######################'
-# 		# end
-#     # puts url
-#     # end
-#   end
