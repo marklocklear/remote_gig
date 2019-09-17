f9c025c1ad05072850ce8c46fc6bc6e3cd274268
Mark Locklear <marklocklear@gmail.com>
2019-09-17 14:39:15 -0400

    add zapier jobs to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,33 @@ require 'nokogiri'
 
 spinner = TTY::Spinner.new("[:spinner] :title")
 jobs_array = Array.new
-spinner.update(title: 'Adding 50 jobs from Stack Overflow...')
+spinner.update(title: 'Adding 20 jobs from Stack Overflow...')
 spinner.auto_spin
 url = "https://stackoverflow.com/jobs/feed?l=Remote"
 doc = Nokogiri::XML(open(url))
 
-doc.xpath('//item')[0..50].each do |item|
+doc.xpath('//item')[0..20].each do |item|
 	title = item.xpath('title').text.split(' at ').first
 	link =	item.xpath('link').text
 	description = Nokogiri::HTML::DocumentFragment.parse(item.xpath('description').text)
 	company = item.xpath('a10:author//a10:name').text
 	jobs_array << [title, link, description, company]
 end
+
+#zapier
+	spinner.update(title: 'Adding jobs from Zapier...')
+	spinner.auto_spin
+	url = "https://zapier.com/jobs/feeds/latest/"
+	doc = Nokogiri::XML(open(url))
+
+	doc.xpath('//item').each do |item|
+		link = item.xpath('link').text
+		title = item.xpath('title').text
+		job_page = Nokogiri::HTML(open(link))
+		description = job_page.xpath('//*[@id="app"]/div/div/div/div/ul[2]')
+		company = 'Zapier'
+		jobs_array << [title, link, description, company]
+	end
 
 #shuffle array (for ramdomness) and create jobs
 spinner.update(title: 'Shuffling and creating jobs...')
