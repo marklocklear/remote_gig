5d717a872f287d3490c41e249d5b3271a4e225de
Mark Locklear <marklocklear@gmail.com>
2019-09-25 12:53:43 -0400

    upgrade ruby version to 2.6.4

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :keep_releases, 5
 set :rvm_type, :user
+set :rvm_ruby_version, '2.6.4' 
 
 namespace :deploy do
 
