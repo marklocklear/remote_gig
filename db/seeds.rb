46c7f32a015f5dab63e8d286975d556391cb0740
Mark Locklear <marklocklear@gmail.com>
2019-10-28 14:11:25 -0400

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,19 +16,33 @@ doc.xpath('//item')[0..20].each do |item|
 end
 
 #zapier
-	spinner.update(title: 'Adding jobs from Zapier...')
-	spinner.auto_spin
-	url = "https://zapier.com/jobs/feeds/latest/"
-	doc = Nokogiri::XML(open(url))
+spinner.update(title: 'Adding jobs from Zapier...')
+spinner.auto_spin
+url = "https://zapier.com/jobs/feeds/latest/"
+doc = Nokogiri::XML(open(url))
 
-	doc.xpath('//item').each do |item|
-		link = item.xpath('link').text
-		title = item.xpath('title').text
-		job_page = Nokogiri::HTML(open(link))
-		description = job_page.xpath('//*[@id="app"]/div/div/div/div/ul[2]')
-		company = 'Zapier'
-		jobs_array << [title, link, description, company]
-	end
+doc.xpath('//item').each do |item|
+	title = item.xpath('title').text
+	link = item.xpath('link').text
+	job_page = Nokogiri::HTML(open(link))
+	description = job_page.xpath('//*[@id="app"]/div/div/div/div/ul[2]')
+	company = 'Zapier'
+	jobs_array << [title, link, description, company]
+end
+
+#7cups
+spinner.update(title: 'Adding jobs from 7cups...')
+spinner.auto_spin
+url = "https://boards.greenhouse.io/embed/job_board?for=7cups"
+doc = Nokogiri::XML(open(url))
+
+doc.css('.opening').each do |char_element|
+	title = char_element.children.text.gsub("Remote", "").delete("\n")
+	link = char_element.children[1][:href]
+	description = link
+	company = '7Cups'
+	jobs_array << [title, link, description, company]
+end
 
 #shuffle array (for ramdomness) and create jobs
 spinner.update(title: 'Shuffling and creating jobs...')
--- lib/sandbox/app.rb
+++ lib/sandbox/app.rb
@@ -1,29 +1,13 @@
 require 'open-uri'
 require 'nokogiri'
 
-doc = Nokogiri::HTML(open("https://skylight.digital/join/"))
-	jobs = doc.css('.open-positions-list')
+url = "https://boards.greenhouse.io/embed/job_board?for=7cups"
+doc = Nokogiri::XML(open(url))
 
-	jobs.css('li').each do |char_element|
-		# puts char_element
-		title = char_element.text
-		# if title.include? 'Remote'
-		# puts "************"
-		# puts title
-    link = char_element.css('a').first['href']
-    # puts link
-    # clickLink = "https://www.hashicorp.com" + link
-    # puts url
-		job_page = Nokogiri::HTML(open(link.to_s))
-		# puts job_page.inspect
-    description = job_page.xpath('/html/body/main/section[3]/ul[1]').text 
-    # puts description
-    	puts title
-      # puts '######################'
-			# puts title + clickLink
-      puts description
-      puts '######################'
-		# end
-    # puts url
-    # end
-  end
+doc.css('.opening').each do |char_element|
+	puts "*********"
+	title = char_element.children.text.gsub("Remote", "").delete("\n")
+	link = char_element.children[1][:href]
+	puts title 
+	puts link
+end
