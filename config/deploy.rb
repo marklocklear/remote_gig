6459bec075afee969787d6483c3b90a765fb5259
Mark Locklear <marklocklear@gmail.com>
2019-10-29 14:50:53 -0400

    update capistrano deploy so nightly_stats text does not get overwritten at deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:marklocklear/remote_gig.git'
 set :deploy_to, '/home/deployer/remotegig'
 set :scm, :git
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public')
 set :keep_releases, 5
 set :rvm_type, :user
 set :rvm_ruby_version, '2.6.4' 
