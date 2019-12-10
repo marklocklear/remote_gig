b56f504659afd7b78ce22cdb5b7e2602a75c9fe1
Mark Locklear <marklocklear@gmail.com>
2019-12-10 12:35:05 -0500

    add sharing via email

---
+++ app/assets/javascripts/job_share.js
@@ -0,0 +1,19 @@
+function myFunction(job_id) {
+  console.log("job id is: ", job_id)
+    document.getElementById("myDropdown-" + job_id).classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
