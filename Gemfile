1d96a7ca18946faad212e99d44136e77f8ccbaf6
Mark Locklear <marklocklear@gmail.com>
2020-01-29 11:05:30 -0500

    add kimurai for better scraping of JS sites and implement in taxjar

--- Gemfile
+++ Gemfile
@@ -70,6 +70,9 @@ gem 'exception_notification'
 #pagination
 gem 'will_paginate'
 
+#allows better web scraping for JS based sites
+gem 'kimurai'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- lib/sites/tax_jar.rb
+++ lib/sites/tax_jar.rb
@@ -1,21 +1,35 @@
 require 'open-uri'
 require 'nokogiri'
+require 'kimurai'
 
-class TaxJar
-	def get_jobs
-		jobs = []
-  	url = "https://taxjar.workable.com"
-    doc = Nokogiri::HTML(open(url))
-    returned_jobs = doc.css('.job')
+class TaxJar < Kimurai::Base
+  @name = "tax_jar"
+  @engine = :selenium_chrome
+  @start_urls = ["https://apply.workable.com/taxjar/"]
 
-    returned_jobs.css('a').each do |char_element|
-      title = char_element.text
-      link = url + char_element['href']
-      job_page = Nokogiri::HTML(open(link.to_s))
-      description = job_page.xpath('/html/body/main/section[3]').text
+  def parse(response, url:, data: {})
+  jobs = Array.new
+
+    doc = browser.current_response
+    returned_jobs = doc.css('.careers-jobs-list-styles__jobsList--3_v12')
+    returned_jobs.css('li').each do |char_element|
+      title = char_element.css('a')[0]['aria-label']
+      link = "https://apply.workable.com" + char_element.css('a')[0]['href']
+
+      #click on job link and get description
+      browser.visit(link)
+      job_page = browser.current_response
+      description = job_page.xpath('/html/body/div[1]/div/div[1]/div[2]/div[2]/div[2]').text
+      # description = link
       company = 'TaxJar'
+      puts "title is: #{title}, link is: #{link}, \n description is: #{description}"
       jobs << [title, link, description, company]
-    end	
-		return jobs
-	end
+    end
+    return jobs
+  end
+
+  def get_jobs
+    jobs = TaxJar.parse!(:parse, url: "https://apply.workable.com/taxjar/")
+    return jobs
+  end
 end
